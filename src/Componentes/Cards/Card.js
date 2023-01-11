2823c4c09a9d8947e611ebb2a4ce8563ea7a87ca
facuh5 <facu5herraez@gmail.com>
2023-01-11 20:24:13 -0300

    boton show more funcionando

--- src/Componentes/Cards/Card.js
+++ src/Componentes/Cards/Card.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import '../Cards/Card.css'
 
 export default function Card({infoPersonaje}){
@@ -7,33 +7,54 @@ export default function Card({infoPersonaje}){
     //         document.querySelector(infoPersonaje.id).classList.add("tarjetaMasAncha")
     //     }
     // })
+
+    let [cartaSimple, setCartaSimple] = useState(true)
+
+    // let cartaSimple = true
+    
+    // function mostrarOcultar() {
+    //     cartaSimple = false
+    // }
+
+    // function mostrar() {
+    //     cartaSimple = true
+    // }
+    function mostrarOcultar() {
+        setCartaSimple(!cartaSimple)
+    }
+
+    // useEffect(()=>{
+    //     setCartaSimple(true)
+    // })
+
     return(
         <div className="tarjetaIndividual" id={"n" + infoPersonaje.id}>
 
             <div className="parteVisible">
                 <img src={infoPersonaje.image}/>
                 <h2 className='nombrePersonaje'>{infoPersonaje.name}</h2>
-                <button className='visible'>Now More...</button>
+                <button className={cartaSimple ? 'visible' : 'invisibilizar'} onClick={mostrarOcultar}>Now More...</button>
             </div>
-
-            <div className='parteInvisible'>
-                <button className='cruz '>X</button>
-                <div className='renglonInformativo'>
-                    <p className='pInfo'>Characters status {infoPersonaje.status}</p>
+            {!cartaSimple?
+                <div className='parteInvisible'>
+                    <button className='cruz ' onClick={mostrarOcultar}>X</button>
+                    <div className='renglonInformativo'>
+                        <p className='pInfo'>Characters status {infoPersonaje.status}</p>
+                    </div>
+                    <div className='renglonInformativo'>
+                        <p className='pTitulo'>Species</p>
+                        <p className='pInfo'>{infoPersonaje.species}</p>
+                    </div>
+                    <div className='renglonInformativo'>
+                        <p className='pTitulo'>Origin</p>
+                        <p className='pInfo'>{infoPersonaje.origin.name}</p>
+                    </div>
+                    <div className='renglonInformativo'>
+                        <p className='pTitulo'>Gender</p>
+                        <p className='pInfo'>{infoPersonaje.gender}</p>
+                    </div>
                 </div>
-                <div className='renglonInformativo'>
-                    <p className='pTitulo'>Species</p>
-                    <p className='pInfo'>{infoPersonaje.species}</p>
-                </div>
-                <div className='renglonInformativo'>
-                    <p className='pTitulo'>Origin</p>
-                    <p className='pInfo'>{infoPersonaje.origin.name}</p>
-                </div>
-                <div className='renglonInformativo'>
-                    <p className='pTitulo'>Gender</p>
-                    <p className='pInfo'>{infoPersonaje.gender}</p>
-                </div>
-            </div>
+            : ''}
         </div>
     )
 }
